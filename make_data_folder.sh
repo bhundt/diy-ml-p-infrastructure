097af956f01a0b5946993424018c6e0da7d8ea7f
Bastian.hundt <bastian@hundt.me>
2022-02-17 14:12:45 +0100

    initial commit

---
+++ make_data_folder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir data
+
+mkdir data/dev
+mkdir data/staging
+mkdir data/prod
+
+mkdir data/dev/features
+mkdir data/dev/raw
+
+mkdir data/staging/features
+mkdir data/staging/raw
+
+mkdir data/prod/features
+mkdir data/prod/raw
