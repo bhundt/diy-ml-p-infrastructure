78005e88988e46eb4ec09a61b9598b2ccb2f8f62
Bastian.hundt <bastian@hundt.me>
2022-02-18 14:39:14 +0100

    folders

--- folders/03_make_pipeline_folder.sh
+++ folders/03_make_pipeline_folder.sh
@@ -2,3 +2,12 @@
 mkdir dev/pipelines/
 mkdir staging/pipelines/
 mkdir prod/pipelines/
+
+mkdir dev/pipelines/etl
+mkdir dev/pipelines/ops
+
+mkdir staging/pipelines/etl
+mkdir staging/pipelines/ops
+
+mkdir prod/pipelines/etl
+mkdir prod/pipelines/ops
